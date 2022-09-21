7dad3784fe538c7deb263799fc033851df7f33ac
Perebati <lucas.bpereira1999@gmail.com>
2022-09-21 18:37:14 -0300

    PlayerMove update

--- Assets/Scripts/PlayerMovementSETA.cs
+++ Assets/Scripts/PlayerMovementSETA.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovementSETA : MonoBehaviour
 {
     [Header("Variaveis de movimentação")]
     float moveSpeed = 5f;
@@ -23,8 +23,8 @@ public class PlayerMovement : MonoBehaviour
 
     void ProcessInputs()
     {
-        float move_x = Input.GetAxisRaw("Horizontal");
-        float move_y = Input.GetAxisRaw("Vertical");
+        float move_x = Input.GetAxisRaw("Horizontal2");
+        float move_y = Input.GetAxisRaw("Vertical2");
 
         movement = new Vector2(move_x, move_y);
     }   

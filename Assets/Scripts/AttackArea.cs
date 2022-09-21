9ac87cf6fec0fd189027b056b2bf8b8cb86c83d6
Luan Munholi <luanmunholi@gmail.com>
2022-09-21 16:33:54 -0300

    merge secundario

---
+++ Assets/Scripts/AttackArea.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackArea : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collider){
+        if (collider.GetComponent<Health>() != null){}
+    }
+}
